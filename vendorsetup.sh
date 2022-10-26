c94bd18a416b1ee6fa12e13ad82e9e88038a8705
Wadie Habib <84630485+wadiehabib@users.noreply.github.com>
2022-10-26 23:09:14 +0300

    clone common kernel for kernel headers

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,7 @@ git clone https://github.com/wadiehabib/vendor_xiaomi_mt6895-common -b 12.1 vend
 git clone https://github.com/wadiehabib/device_xiaomi_mt6895-common -b 12.1 device/xiaomi/mt6895-common
 git clone https://github.com/wadiehabib/vendor_xiaomi_rubens -b oss vendor/xiaomi/rubens
 git clone https://github.com/PixelExperience/device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+git clone https://github.com/wbs306/kernel_xiaomi_mt6895 -b 12.1 kernel/xiaomi/mt6895
 cd ~/anc/frameworks/opt/telephony
 git fetch https://gerrit.pixelexperience.org/frameworks_opt_telephony refs/changes/76/15476/1 && git cherry-pick FETCH_HEAD
 cd ~/anc/frameworks/opt/net/ims
